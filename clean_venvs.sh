41be02b1c4880cd8c2177719a28f5a92cbfe701d
y-hashida <49150386+y-hashida@users.noreply.github.com>
2021-11-22 00:48:44 +0900

    やっぱりこのディレクトリいらない.

--- clean_venvs.sh
+++ clean_venvs.sh
@@ -4,6 +4,6 @@
 REPO_DIR="$(cd $(dirname ${0}); pwd)"
 
 ## _venvs の _venvs の中身を削除
-rm -rf "${REPO_DIR}/"{_venvs,tmp,cmds}
-mkdir -p "${REPO_DIR}/"{_venvs,tmp,cmds}
-touch "${REPO_DIR}/"{_venvs,tmp,cmds}"/.gitkeep"
+rm -rf "${REPO_DIR}/"{_venvs,tmp}
+mkdir -p "${REPO_DIR}/"{_venvs,tmp}
+touch "${REPO_DIR}/"{_venvs,tmp}"/.gitkeep"
--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ set -eu
 ## Setting
 REPO_DIR="$(cd $(dirname ${0}) && pwd)"
 SCRIPT_DIR="$(cd ${REPO_DIR}/_scripts && pwd)"
-CMD_DIR="$(cd ${REPO_DIR}/cmds && pwd)"
 
 ## コマンドリスト
 COMMAND_LIST=$(cat << EOF
@@ -19,8 +18,3 @@ EOF
 
 ## Install conda and python
 bash "${SCRIPT_DIR}/wrapper.sh"
-
-# ## Create symbolic link
-# for COMMAND in ${COMMAND_LIST}; do
-#     ln -sf "${SCRIPT_DIR}/wrapper.sh" "${CMD_DIR}/${COMMAND}"
-# done
